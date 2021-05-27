7e25147c7162ca898767a08b284e1c89cd2f60af
Gediminas Šimavičius <gediminas.simavicius@gmail.com>
2021-05-28 00:37:12 +0300

    Started to create Scraper class

---
+++ value_today/scraper.py
@@ -0,0 +1,26 @@
+class Scraper:
+    """
+    A class used for scraping information from value.today website.
+
+    ...
+
+    Attributes
+    ----------
+    None
+
+    Methods
+    -------
+    number_companies():
+        Returns the number of how much companies could be scraped.
+    valuetoday_usa_scraper(n_companies):
+        Returns dataframe with financial data of companies.
+    df_cleaning(raw_df):
+        Returns cleaned data frame.
+    save_csv(data, file_name):
+        Save dataframe to .csv file.
+    """ 
+
+    def __init__(self) -> None:
+        pass
+
+    
